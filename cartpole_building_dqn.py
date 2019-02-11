e321e187c05f7b474d5f16e9634f5392024a410d
Harshit Maheshwari <harshitmah98@gmail.com>
2019-02-11 23:30:15 +0530

    Built CartPole environment

--- cartpole_building_dqn.py
+++ cartpole_building_dqn.py
@@ -38,6 +38,7 @@ class Agent():
         if os.path.isfile(self.weight_backup):
             model.load_weights(self.weight_backup)
             self.exploration_rate = self.exploration_min
+        return model
             
     def save_model(self):
         self.brain.save(self.weight_backup)
@@ -64,6 +65,44 @@ class Agent():
             self.brain.fit(state, target_f, epochs=1,verbose=0)
         if self.exploration_rate > self.exploration_min:
             self.exploration_rate *= self.exploration_decay
+            
+class CartPole:
+    def __init__(self):
+        self.sample_batch_size = 32
+        self.episodes = 10000
+        self.env = gym.make('CartPole-v1')
+        self.state_size = self.env.observation_space.shape[0]
+        self.action_size = self.env.action_space.n
+        self.agent = Agent(self.state_size, self.action_size)
+        
+    def run(self):
+        try:
+            for index_episode in range(self.episodes):
+                state = self.env.reset()
+                state = np.reshape(state, [1, self.state_size])
+                done = False
+                index = 0
+                
+                while not done:
+                    #self.env.render()
+                    action = self.agent.act(state)
+                    next_state, reward, done, _ = self.env.step(action)
+                    next_state = np.reshape(next_state, [1, self.state_size])
+                    self.agent.remember(state, action, reward, next_state, done)
+                    state = next_state
+                    index += 1
+                print("Episode: {}\tScore: {}".format(index_episode, index+1))
+                self.agent.replay(self.sample_batch_size)
+        finally:
+            self.agent.save_model()
+            
+if __name__ == "__main__":
+    cartpole = CartPole()
+    cartpole.run()
+                    
+                
+        
+    
     
     
         
